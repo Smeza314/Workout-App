b42b6ba21fafc82b297948133bf763022e785fb2
Smeza314 <sebastianmeza314@gmail.com>
2021-04-25 13:38:22 -0700

    updates

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,15 +1,16 @@
 const router = require('express').Router()
 const { join } = require('path')
 
-router.get('/exercise', (req, res) => {
-  res.sendFile(join(__dirname, '../public/exercise.html'))
-})
 
 router.get('/stats', (req, res) => {
   res.sendFile(join(__dirname, '../public/stats.html'))
 })
 
-router.get('/*', (res, req) => {
+router.get('/exercise', (req, res) => {
+  res.sendFile(join(__dirname, '../public/exercise.html'))
+})
+
+router.get('/', (req, res) => {
   res.sendFile(join(__dirname, '../public/index.html'))
 })
 
